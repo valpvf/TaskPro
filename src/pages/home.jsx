04da9b6b08065875172cef0803f33b14eeef8046
Valerii Pozhydaiev <p_vf@i.ua>
2023-07-26 23:00:15 +0300

    correct

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,17 @@
+import Header from 'components/Header/Header';
+import ScreensPage from 'components/ScreensPage/ScreensPage';
+import Sidebar from 'components/Sidebar/Sidebar';
+
 const Home = () => {
-  return <h1>Tasks page</h1>;
+  return (
+    <>
+      <Sidebar />
+      <>
+        <Header />
+        <ScreensPage />
+      </>
+    </>
+  );
 };
 
 export default Home;
