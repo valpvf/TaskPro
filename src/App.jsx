754184327ef5ea7d22b4bceac6db690ceaa6ee49
Valerii Pozhydaiev <p_vf@i.ua>
2023-07-27 01:06:28 +0300

    verify

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import Home from 'pages/Home';
-// import Welcome from 'pages/Welcome';
-// import { Suspense } from 'react';
-// import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from 'components/Home';
+import Welcome from 'components/Welcome';
+import { Suspense } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 // const Welcome = lazy(() => import('./pages/Welcome'));
 // const Home = lazy(() => import('./pages/Home'));
 
 export const App = () => {
   return (
-    <Home />
-    // <Suspense fallback={<h1>Loading ...</h1>}>
-    //   <Routes>
-    //     <Route path="/" element={<Welcome />} />
-    //     <Route index element={<Welcome />} />
-    //     <Route path="home" element={<Home />} />
-    //     <Route path="*" element={<Navigate to="/" />} />
-    //   </Routes>
-    // </Suspense>
+    <>
+      <Home />
+      <Suspense fallback={<h1>Loading ...</h1>}>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route index element={<Welcome />} />
+          <Route path="home" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
 
