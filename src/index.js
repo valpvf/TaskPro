eb472eadcf3af7a08c1ede260859401bacda3853
Olena Korenieva <olena.korenieva@gmail.com>
2023-07-27 10:31:59 +0200

    added general styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-import './index.css';
+import './index.scss';
 import { App } from 'App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
