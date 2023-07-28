dcc617d883015d43df5d5ccf5752b0d981a1454a
Dan Dudaiev <122373927+UiMi17@users.noreply.github.com>
2023-07-28 19:48:27 +0300

    ADDED: radio images.

--- src/shared/components/radioButtons/RadioColored.jsx
+++ src/shared/components/radioButtons/RadioColored.jsx
@@ -1,44 +1,101 @@
-import { FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
+import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
 
 const RadioColored = () => {
   return (
-    <form>
-      <FormLabel
-        id="label-changer"
-        sx={{
-          fontFamily: 'Poppins',
-          fontSize: '12px',
-          letterSpacing: '-0.24px',
-          border: 'none',
-          color: 'rgba(255, 255, 255, 0.50);',
-        }}
-      >
-        Label color
-      </FormLabel>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        width: '140px',
+        margin: '0 auto',
+      }}
+    >
       <RadioGroup
         aria-labelledby="label-changer"
         defaultValue="gray"
         name="radio-buttons-group"
+        sx={{ display: 'flex', gap: '8px' }}
         row
       >
-        <FormControlLabel value="blue" control={<Radio />} />
-        <FormControlLabel value="pink" control={<Radio />} />
-        <FormControlLabel value="green" control={<Radio />} />
         <FormControlLabel
-          value="gray"
+          value="blue"
+          sx={{ margin: '0' }}
           control={
             <Radio
               sx={{
-                color: '#111',
                 width: '14px',
                 height: '14px',
-                backgroundColor: 'rgba(255, 255, 255, 0.30);',
+                color: 'transparent',
+                backgroundColor: 'var(--8FA1D0)',
+                '&.Mui-checked': {
+                  color: 'var(--8FA1D0)',
+                  backgroundColor: '#151512',
+                },
               }}
+              disableRipple
+            />
+          }
+        />
+        <FormControlLabel
+          value="pink"
+          sx={{ margin: '0' }}
+          control={
+            <Radio
+              sx={{
+                width: '14px',
+                height: '14px',
+                color: 'transparent',
+                backgroundColor: 'var(--E09CB5)',
+                '&.Mui-checked': {
+                  color: 'var(--E09CB5)',
+                  backgroundColor: '#151512',
+                },
+              }}
+              disableRipple
+            />
+          }
+        />
+        <FormControlLabel
+          value="green"
+          sx={{ margin: '0' }}
+          control={
+            <Radio
+              sx={{
+                width: '14px',
+                height: '14px',
+                color: 'transparent',
+                backgroundColor: 'var(--BEDBB0)',
+                '&.Mui-checked': {
+                  color: 'var(--BEDBB0)',
+                  backgroundColor: '#151512',
+                },
+              }}
+              disableRipple
+            />
+          }
+        />
+        <FormControlLabel
+          value="gray"
+          sx={{ margin: '0' }}
+          control={
+            <Radio
+              sx={{
+                width: '14px',
+                height: '14px',
+                color: 'transparent',
+                backgroundColor: 'rgba(255, 255, 255, 0.30)',
+                '&.Mui-checked': {
+                  color: 'rgba(255, 255, 255, 0.30)',
+                  backgroundColor: '#151512',
+                },
+              }}
+              disableRipple
             />
           }
         />
       </RadioGroup>
-    </form>
+    </div>
   );
 };
 
---
+++ src/shared/components/radioButtons/RadioImages.styled.jsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const StyledRadioWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
+  width: 252px;
+  height: 60px;
+`;
+
+export const StyledLabel = styled.label`
+  & img {
+    cursor: pointer;
+  }
+`;
+
+export const StyledInput = styled.input`
+  position: absolute;
+  opacity: 0;
+  width: 0;
+  height: 0;
+
+  &:checked + img {
+    transform: scale(1.2);
+    transition: transform 280ms ease-in;
+  }
+`;
