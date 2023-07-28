741eb0b4a19276de13ba1b675edf2e22a6debf1b
Valerii Pozhydaiev <p_vf@i.ua>
2023-07-29 02:52:32 +0300

    cleaning code

--- src/components/ModalNeedHelp/ModalNeedHelp.jsx
+++ src/components/ModalNeedHelp/ModalNeedHelp.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import Modal from 'components/Modal/Modal';
 import ModalTitle from 'components/ModalTitle/ModalTitle';
