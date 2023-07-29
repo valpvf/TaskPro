d03e4b000e5c24a6d2036ed943480944b4c837df
valpvf <Valerii.Pozhydaiev@gmail.com>
2023-07-29 08:38:24 +0000

    Deploying to gh-pages from @ valpvf/taskspro@69ff24237eacb8607887162b60826d41c4ca0c08 🚀

--- static/js/975.d2df6421.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktaskspro=self.webpackChunktaskspro||[]).push([[975],{5975:function(e,r,s){s.r(r);var t=s(1087),n=s(3329);r.default=function(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("h1",{children:"Start page"}),(0,n.jsx)(t.rU,{to:"/home",children:"To Home Page"})]})}}}]);
-//# sourceMappingURL=975.d2df6421.chunk.js.map
