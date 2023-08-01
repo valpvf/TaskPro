ab3a76f12048d29a10cab7585f3808af8d19b177
Valerii Pozhydaiev <p_vf@i.ua>
2023-08-01 18:44:20 +0300

    correct conflict with main

--- src/redux/task/taskOperations.js
+++ src/redux/task/taskOperations.js
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { addColumnApi } from '../../services/backApi';
 
-// export const getBoard = createAsyncThunk('task/getBoard', async () => {
-//   //   console.log(1);
-//   const response = await getBoardApi('64c7c7d5e2d3e2b069fe01e9');
-//   console.log(2);
-//   return response.data;
-// });
+export const getBoard = createAsyncThunk('task/getBoard', async () => {
+  //   //   console.log(1);
+  //   const response = await getBoardApi('64c7c7d5e2d3e2b069fe01e9');
+  //   console.log(2);
+  //   return response.data;
+});
 
 export const addColumn = createAsyncThunk(
   'column/add',
