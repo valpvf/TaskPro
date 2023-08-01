20a8332fc8afc8c933b350aa1bd6a3c214adbad1
AlinaMarchuk <123773400+AlinaMarchuk@users.noreply.github.com>
2023-08-01 15:14:32 +0300

    open/close Edit Profile

--- src/components/ModalEditProfile/ModalEditProfile.jsx
+++ src/components/ModalEditProfile/ModalEditProfile.jsx
@@ -1,14 +1,11 @@
 import Modal from 'components/Modal/Modal';
 import ModalTitle from 'components/ModalTitle/ModalTitle';
-import ButtonMain from 'shared/components/button/Button';
 
 const ModalEditProfile = ({ onClose }) => {
   return (
     <Modal onClose={onClose}>
       <ModalTitle>Edit profile</ModalTitle>
-      <form>
-        <ButtonMain type="submit">Send</ButtonMain>
-      </form>
+      <form></form>
     </Modal>
   );
 };
