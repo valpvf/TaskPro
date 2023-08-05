9c463b53bc6ec74ea94807a61849ca9ffdd0b6e8
Dan Dudaiev <122373927+UiMi17@users.noreply.github.com>
2023-08-05 16:00:15 +0300

    ADDED: filters modal close on radio change

--- src/components/ModalFilters/ModalFilters.jsx
+++ src/components/ModalFilters/ModalFilters.jsx
@@ -1,15 +1,11 @@
 import Modal from 'components/Modal/Modal';
 import ModalTitle from 'components/ModalTitle/ModalTitle';
 import RadioFilters from 'shared/components/radioButtons/RadioFilters';
-import {
-  FormStyled,
-  BoxStyled,
-  SubtitleStyled,
-} from './ModalFilters.styled';
+import { FormStyled, BoxStyled, SubtitleStyled } from './ModalFilters.styled';
 import { useState } from 'react';
 
 const ModalFilters = ({ onClose }) => {
-  const [priority, setPriority] = useState('none')
+  const [priority, setPriority] = useState('none');
 
   console.log(priority);
   return (
@@ -19,7 +15,7 @@ const ModalFilters = ({ onClose }) => {
         <BoxStyled>
           <SubtitleStyled>Label color</SubtitleStyled>
         </BoxStyled>
-        <RadioFilters onFilterChange={setPriority}/>
+        <RadioFilters onFilterChange={setPriority} onModalClose={onClose} />
       </FormStyled>
     </Modal>
   );
