6e896a98fa5b7b5f0909c54bccac17b5775ca8e3
Dan Dudaiev <122373927+UiMi17@users.noreply.github.com>
2023-08-05 14:24:46 +0300

    ADDED: -;

--- src/shared/components/radioButtons/RadioColored.jsx
+++ src/shared/components/radioButtons/RadioColored.jsx
@@ -1,6 +1,37 @@
 import { Box, FormControlLabel, Radio, RadioGroup } from '@mui/material';
 
 const RadioColored = ({ onRadioChange, currentPriority }) => {
+  const radioButtons = [
+    {
+      value: 'High',
+      radioColor: {
+        static: 'var(--radio-label-background-color)',
+        checked: 'var(--radio-label-checked-background-color)',
+        additional: 'var(--radio-label-checked-background-color)',
+      },
+    },
+    {
+      value: 'Medium',
+      radioColor: {
+        static: 'var(--radio-label-pink-color)',
+        checked: 'var(--radio-label-checked-background-color)',
+      },
+    },
+    {
+      value: 'Low',
+      radioColor: {
+        static: 'var(--radio-label-green-color)',
+        checked: 'var(--radio-label-checked-background-color)',
+      },
+    },
+    {
+      value: 'Without',
+      radioColor: {
+        static: 'var(--radio-label-grey-color)',
+        checked: 'var(--radio-label-checked-background-color)',
+      },
+    },
+  ];
   return (
     <Box
       sx={{
@@ -18,96 +49,32 @@ const RadioColored = ({ onRadioChange, currentPriority }) => {
         sx={{ display: 'flex', gap: '8px' }}
         row
       >
-        <FormControlLabel
-          value="High"
-          sx={{ margin: '0' }}
-          control={
-            <Radio
-              sx={{
-                width: '14px',
-                height: '14px',
-                color: 'transparent',
+        {radioButtons.map(button => {
+          const { value, radioColor } = button;
+          return (
+            <FormControlLabel key={value}
+              value={value}
+              sx={{ margin: '0' }}
+              control={
+                <Radio
+                  sx={{
+                    width: '14px',
+                    height: '14px',
+                    color: 'transparent',
 
-                backgroundColor: 'var(--radio-label-background-color)',
-                '&.Mui-checked': {
-                  color: 'var(--radio-label-color)',
+                    backgroundColor: radioColor.static,
+                    '&.Mui-checked': {
+                      color: radioColor.static,
 
-                  backgroundColor:
-                    'var(--radio-label-checked-background-color)',
-                },
-              }}
-              disableRipple
+                      backgroundColor: radioColor.checked,
+                    },
+                  }}
+                  disableRipple
+                />
+              }
             />
-          }
-        />
-        <FormControlLabel
-          value="Medium"
-          sx={{ margin: '0' }}
-          control={
-            <Radio
-              sx={{
-                width: '14px',
-                height: '14px',
-                color: 'transparent',
-                //
-                backgroundColor: 'var(--radio-label-pink-color)',
-                '&.Mui-checked': {
-                  color: 'var(--radio-label-pink-color)',
-
-                  backgroundColor:
-                    'var(--radio-label-checked-background-color)',
-                },
-              }}
-              disableRipple
-            />
-          }
-        />
-        <FormControlLabel
-          value="Low"
-          sx={{ margin: '0' }}
-          control={
-            <Radio
-              sx={{
-                width: '14px',
-                height: '14px',
-                color: 'transparent',
-                //
-                backgroundColor: 'var(--radio-label-green-color)',
-
-                '&.Mui-checked': {
-                  color: 'var(--radio-label-green-color)',
-
-                  backgroundColor:
-                    'var(--radio-label-checked-background-color)',
-                },
-              }}
-              disableRipple
-            />
-          }
-        />
-        <FormControlLabel
-          value="Without"
-          sx={{ margin: '0' }}
-          control={
-            <Radio
-              sx={{
-                width: '14px',
-                height: '14px',
-                color: 'transparent',
-
-                backgroundColor: 'var(--radio-label-grey-color)',
-                '&.Mui-checked': {
-                  // c
-                  color: 'var(--radio-label-grey-color)',
-
-                  backgroundColor:
-                    'var(--radio-label-checked-background-color)',
-                },
-              }}
-              disableRipple
-            />
-          }
-        />
+          );
+        })}
       </RadioGroup>
     </Box>
   );
