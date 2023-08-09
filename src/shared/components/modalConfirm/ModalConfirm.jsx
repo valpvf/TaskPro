5d60de4398b90bb3b2d501048bf51b41887c28cf
Olena Korenieva <olena.korenieva@gmail.com>
2023-08-09 11:43:09 +0200

    added fixes of modal Confirm and fixed header styles

--- src/shared/components/modalConfirm/ModalConfirm.jsx
+++ src/shared/components/modalConfirm/ModalConfirm.jsx
@@ -2,7 +2,7 @@ import Modal from 'components/Modal/Modal';
 import ModalTitle from 'components/ModalTitle/ModalTitle';
 import { Wrapper, ButtonStyled } from './ModalConfirm.styled';
 
-const ModalConfirm = ({ onConfirm, onClose }) => {
+const ModalConfirm = ({ onConfirm, onClose, title = 'Are you sure?' }) => {
   const handleConfirm = () => {
     onConfirm();
   };
@@ -13,7 +13,7 @@ const ModalConfirm = ({ onConfirm, onClose }) => {
 
   return (
     <Modal onClose={onClose}>
-      <ModalTitle>Are you sure?</ModalTitle>
+      <ModalTitle>{title}</ModalTitle>
       <Wrapper>
         <ButtonStyled type="button" onClick={handleConfirm}>
           Yes
