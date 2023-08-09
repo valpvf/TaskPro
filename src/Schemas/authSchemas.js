9b79aa0e4cd77e453918c65f17bb6e748844bf32
Yaroslav Klimas <yarklim@gmail.com>
2023-08-09 10:50:54 +0300

    changed validation schemas

--- src/Schemas/authSchemas.js
+++ src/Schemas/authSchemas.js
@@ -2,20 +2,51 @@ import * as Yup from 'yup';
 
 export const RegisterSchema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is required')
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: 'Email error',
+      excludeEmptyString: true,
+    }),
   password: Yup.string()
     .min(8, 'Password must be at least 8 characters')
-    .required('Password is required'),
+    .required('Password is required')
+    .matches(/^[a-zA-Z0-9]+$/, {
+      message:
+        'Password must contain Latin letters and numbers, without spaces in different case, from 8 to 64 characters',
+      excludeEmptyString: true,
+    }),
 });
 
 export const LoginSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Email is required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is required')
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: 'Email error',
+      excludeEmptyString: true,
+    }),
   password: Yup.string()
     .min(8, 'Password must be at least 8 characters')
-    .required('Password is required'),
+    .required('Password is required')
+    .matches(/^[a-zA-Z0-9]+$/, {
+      message: 'Password error',
+      excludeEmptyString: true,
+    }),
 });
 
 export const UpdateUserSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email'),
-  password: Yup.string().min(8, 'Password must be at least 8 characters'),
+  email: Yup.string()
+    .email('Invalid email')
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: 'Email error',
+      excludeEmptyString: true,
+    }),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .matches(/^[a-zA-Z0-9]+$/, {
+      message: 'Password error',
+      excludeEmptyString: true,
+    }),
 });
