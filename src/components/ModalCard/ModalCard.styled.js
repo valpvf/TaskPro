29cfb67601b8d4e139b9a885aa6b5d54aa399003
Valerii Pozhydaiev <p_vf@i.ua>
2023-08-12 23:42:34 +0300

    cleaning code part 3 + add Loader on Register/Login/Logout

--- src/components/ModalCard/ModalCard.styled.js
+++ src/components/ModalCard/ModalCard.styled.js
@@ -1,9 +1,9 @@
-import styled from '@emotion/styled';
 import { Field } from 'formik';
-import 'react-datepicker/dist/react-datepicker.css';
-import '../../shared/basicStyles/variables.scss';
 import Svg from '../../images/chevron-down.svg';
 import SvgBlue from '../../images/chevron-down-blue.svg';
+import styled from '@emotion/styled';
+import 'react-datepicker/dist/react-datepicker.css';
+import '../../shared/basicStyles/variables.scss';
 
 export const TextareaStyled = styled(Field)`
   width: 100%;
@@ -11,25 +11,24 @@ export const TextareaStyled = styled(Field)`
   margin-top: 18px;
   padding: 14px 18px;
   resize: none;
-  border-radius: 8px;
-  border: 1px solid var(--textarea-border-color);
-  background-color: transparent;
   transition: border 250ms ease-in;
-
+  letter-spacing: -0.28px;
   color: var(--modal-board-text);
+  border: 1px solid var(--textarea-border-color);
+  border-radius: 8px;
+  background-color: transparent;
   font-family: 'Poppins', sans-serif;
   font-size: 14px;
   font-weight: 400;
   line-height: 1.3;
-  letter-spacing: -0.28px;
 
   ::placeholder {
+    letter-spacing: -0.28px;
     color: var(--modal-board-text);
     font-family: 'Poppins', sans-serif;
     font-size: 14px;
     font-weight: 400;
     line-height: 1.3;
-    letter-spacing: -0.28px;
   }
 
   &:hover,
@@ -42,23 +41,23 @@ export const TextareaStyled = styled(Field)`
 
 export const SubtitleStyled = styled.p`
   margin: 24px 0 4px 0;
+  letter-spacing: -0.24px;
   color: var(--datepicker-dayname-color);
   font-family: 'Poppins', sans-serif;
   font-size: 12px;
   font-weight: 400;
   line-height: 1.5;
-  letter-spacing: -0.24px;
 `;
 
 export const LabelStyled = styled.label`
   display: block;
   margin: 14px 0 4px 0;
+  letter-spacing: -0.24px;
   color: var(--datepicker-dayname-color);
   font-family: 'Poppins', sans-serif;
   font-size: 12px;
   font-weight: 400;
   line-height: 1.5;
-  letter-spacing: -0.24px;
 `;
 
 export const DatePickerWrapper = styled.div`
@@ -69,40 +68,39 @@ export const DatePickerWrapper = styled.div`
     position: relative;
     width: 100%;
     padding: 0;
+    cursor: pointer;
+    letter-spacing: -0.28px;
+    color: var(--primary-modal-card-color);
+    border: none;
+    outline: none;
     background-image: ${props =>
       props.violet ? `url(${SvgBlue})` : `url(${Svg})`};
+    background-color: transparent;
     background-repeat: no-repeat;
     background-position: top 6px right 65px;
-
     font-family: 'Poppins', sans-serif;
     font-size: 14px;
     font-weight: 500;
     line-height: 1.5;
-    letter-spacing: -0.28px;
-    background-color: transparent;
-    border: none;
-    outline: none;
-    color: var(--primary-modal-card-color);
-    cursor: pointer;
   }
   & .react-datepicker__triangle {
     display: none;
   }
   & .react-datepicker {
-    font-family: 'Poppins', sans-serif;
-    background-color: var(--datepicker-background-color);
-    border-radius: 6px;
     border: 1px solid var(--primary-modal-card-color);
+    border-radius: 6px;
+    background-color: var(--datepicker-background-color);
+    font-family: 'Poppins', sans-serif;
   }
   & .react-datepicker__header {
     background-color: var(--datepicker-background-color);
   }
   & .react-datepicker__current-month {
+    letter-spacing: -0.32px;
+    color: var(--datepicker-current-month);
     font-family: 'Poppins', sans-serif;
     font-size: 16px;
     line-height: 1.5;
-    letter-spacing: -0.32px;
-    color: var(--datepicker-current-month);
   }
   & .react-datepicker__navigation-icon--next {
     background-color: var(--primary-modal-card-color);
@@ -114,39 +112,37 @@ export const DatePickerWrapper = styled.div`
     font-weight: 500;
   }
   & .react-datepicker__day {
-    color: var(--datepicker-current-day);
-    background-color: transparent;
-    border-radius: 50%;
     transition: background-color 250ms ease-in, color 250ms ease-in;
-
+    color: var(--datepicker-current-day);
+    border-radius: 50%;
+    background-color: transparent;
     font-family: 'Poppins', sans-serif;
     font-size: 14px;
     font-weight: 400;
   }
   & .react-datepicker__day--selected {
+    color: var(--datepicker-background-color);
     background-color: var(--primary-modal-card-color);
     border-radius: 50%;
-    color: var(--datepicker-background-color);
   }
   & .react-datepicker__day--disabled {
     color: var(--datepicker-disabled-day);
   }
   & .react-datepicker__day:hover {
-    background-color: var(--datepicker-hover-background-color);
     color: var(--datepicker-background-color);
+    background-color: var(--datepicker-hover-background-color);
   }
   & .react-datepicker__day--disabled:hover {
-    background-color: transparent;
-    color: rgba(255, 255, 255, 0.2);
     color: var(--datepicker-hover-disabled-day);
+    background-color: transparent;
   }
 `;
 
 export const SpanStyled = styled.span`
+  letter-spacing: -0.28px;
+  color: var(--primary-modal-card-color);
   font-family: 'Poppins', sans-serif;
   font-size: 14px;
   font-weight: 500;
   line-height: 1.5;
-  letter-spacing: -0.28px;
-  color: var(--primary-modal-card-color);
 `;
